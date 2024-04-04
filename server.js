12358c3069cf1778ff37dc587d605a77bd7692eb
nihaljijonihal <nihaljijonihal@gmail.com>
2024-04-04 21:40:24 +0530

    update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = createServer(app);
 const io = socket(server, {
   cors: {
-    origin: "*",
+    origin: "https://simple-chat-app-frontend.vercel.app/",
   },
 });
 
