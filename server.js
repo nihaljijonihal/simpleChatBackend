9f95d0409a58cdd1b6118b5eff6b6158a8605fd5
nihaljijonihal <nihaljijonihal@gmail.com>
2024-04-04 21:17:28 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const { createServer } = require("http");
+const socket = require("socket.io");
+
+const PORT = 5000 || process.env.PORT
+const app = express();
+const server = createServer(app);
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+const userNameArray = [];
+const userIdArray = [];
+
+// const io = new server (server)
+
+io.on("connection", (socket) => {
+  socket.on("chatJoin", ({ userName, userId }) => {
+      
+    const newUserId = userId;
+    const newUserName = userName;
+      
+    userNameArray[socket] = newUserName;
+    userIdArray[socket] = newUserId;
+    console.log(`New User Id ${newUserId} and Name ${newUserName}`);
+    io.emit("newUserToast", { newUserName, newUserId });
+    
+  });
+
+  socket.on("chat", (payload) => {
+    io.emit("chat", payload);
+    
+   
+  });
+
+  socket.on("disconnect", () => {
+    const leftUserId = userIdArray[socket];
+    const leftUserName = userNameArray[socket];
+    console.log(`Use Left Id :${leftUserId} Name :${leftUserName}`);
+    io.emit("userLeftToast", { leftUserName, leftUserId });
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is listening at port ${PORT}...`);
+});
